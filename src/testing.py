2a4c024985d5ea77214045f23c4b0d69124d2dc2
LinirZamir <alzcorp@gmail.com>
2022-10-09 02:58:36 -0400

    Included GPT

--- src/testing.py
+++ src/testing.py
@@ -1,9 +1,18 @@
 import os 
 from dotenv import load_dotenv
-import openai
+from gpt import GPT, Example, set_openai_key
 
 ## Loading .env file with data
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-response = openai.Completion.create(model="text-davinci-002", prompt="Say this is a test", temperature=0, max_tokens=6)
+api_key = os.getenv("OPENAI_API_KEY")
+set_openai_key(api_key)
+gpt = GPT()
+prompt = "integral from a to b of f of x"
+print(gpt.get_top_reply(prompt))
+
+gpt.add_example(Example("Two plus two equals four", "2 + 2 = 4"))
+print(gpt.get_top_reply(prompt))
+
+gpt.add_example(Example('The integral from zero to infinity', '\\int_0^{\\infty}'))
+print(gpt.get_top_reply(prompt))
